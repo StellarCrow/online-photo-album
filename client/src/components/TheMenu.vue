be1e67d4088ccc0b01d8d271b12d3da1fb3eb5d5
StellarCrow <dariadar12@gmail.com>
2019-12-17 14:20:10 +0200

    responsive nav menu

--- client/src/components/TheMenu.vue
+++ client/src/components/TheMenu.vue
@@ -1,18 +1,26 @@
 <template>
-  <nav class="menu">
-    <router-link class="menu__item" to="/">Лого</router-link>
-    <div class="menu__item dropdown">
-      <span>Категории фотографий</span>
-      <i class="dropdown__icon"></i>
-      <div class="dropdown__content">
-        <router-link class="dropdown__item" to="#">Популярные</router-link>
-        <router-link class="dropdown__item" to="#">Новые</router-link>
+  <nav class="navigation">
+    <div navigation__logo>Logo</div>
+
+    <input type="checkbox" id="navigation_icon"/>
+
+    <div class="navigation__menu menu">
+      <div class="menu__item dropdown">
+        <span>Категории фотографий</span>
+        <i class="dropdown__icon"><font-awesome-icon :icon="['fa', 'caret-down']"></font-awesome-icon></i>
+        <div class="dropdown__content">
+          <router-link class="dropdown__item" to="#">Популярные</router-link>
+          <router-link class="dropdown__item" to="#">Новые</router-link>
+        </div>
       </div>
+      <router-link class="menu__item" to="#">Случайное фото</router-link>
+      <router-link class="menu__item" to="#">Контакты</router-link>
+      <router-link class="menu__item" to="/about">О нас</router-link>
     </div>
-    <router-link class="menu__item" to="#">Случайное фото</router-link>
-    <router-link class="menu__item" to="#">Контакты</router-link>
-    <router-link class="menu__item" to="/about">О нас</router-link>
-    <div class="menu__item">Поиск</div>
+    
+    <div class="navigation__search">Search</div>
+
+    <label for="navigation_icon" class="navigation__icon"><font-awesome-icon :icon="['fas', 'bars']"></font-awesome-icon></label>
   </nav>
 </template>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,16 @@
+// ------------------IMPORTS-----------------------
+
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCaretDown, faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// -----------------------------------------------------
+library.add(faCaretDown, faBars);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
