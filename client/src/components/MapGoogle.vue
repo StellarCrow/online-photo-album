a19109a281d8fe6276d956f7f143775a04113b7a
StellarCrow <dariadar12@gmail.com>
2019-12-22 00:30:17 +0200

    basic google maps on contact page

---
+++ client/src/components/MapGoogle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="map"></div>
+</template>
+
+<script>
+import gmapsInit from "../utils/gmaps";
+
+export default {
+  name: "Map",
+  async mounted() {
+    try {
+      const google = await gmapsInit();
+      const geocoder = new google.maps.Geocoder();
+      const map = new google.maps.Map(this.$el);
+
+      geocoder.geocode({ address: "Austria" }, (results, status) => {
+        if (status !== "OK" || !results[0]) {
+          throw new Error(status);
+        }
+
+        map.setCenter(results[0].geometry.location);
+        map.fitBounds(results[0].geometry.viewport);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.map {
+  height: 50vh;
+  width: 50vw;
+}
+</style>
--- client/src/views/Contacts.vue
+++ client/src/views/Contacts.vue
@@ -1,9 +1,19 @@
 <template>
-  <div></div>
+  <div>
+      Hello
+    <Map />
+  </div>
 </template>
 
 <script>
-export default {};
+import Map from "../components/MapGoogle";
+
+export default {
+  name: "Contacts",
+  components: {
+    Map
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
