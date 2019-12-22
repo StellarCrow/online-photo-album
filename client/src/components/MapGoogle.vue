b351142785992547f93157c4f3358c1245b30d77
StellarCrow <dariadar12@gmail.com>
2019-12-22 18:48:58 +0200

    added markers on the map

--- client/src/components/MapGoogle.vue
+++ client/src/components/MapGoogle.vue
@@ -13,7 +13,15 @@ export default {
       const geocoder = new google.maps.Geocoder();
       const map = new google.maps.Map(this.$el);
 
-      geocoder.geocode({ address: "Austria" }, (results, status) => {
+      map.setZoom(20);
+
+      var coordinates = {
+        location1: { lat: 50.45, lng: 30.467 },
+        location2: { lat: 50.466, lng: 30.508 },
+        location3: { lat: 50.43, lng: 30.515 }
+      };
+
+      geocoder.geocode({ address: "Kyiv" }, (results, status) => {
         if (status !== "OK" || !results[0]) {
           throw new Error(status);
         }
@@ -21,6 +29,23 @@ export default {
         map.setCenter(results[0].geometry.location);
         map.fitBounds(results[0].geometry.viewport);
       });
+
+      var marker = new google.maps.Marker({
+        position: coordinates.location1,
+        title: "OPA Политехнический институт"
+      });
+      var marker2 = new google.maps.Marker({
+        position: coordinates.location2,
+        title: "OPA Контрактовая площадь"
+      });
+      var marker3 = new google.maps.Marker({
+        position: coordinates.location3,
+        title: "OPA Олимпийская"
+      });
+
+      marker.setMap(map);
+      marker2.setMap(map);
+      marker3.setMap(map);
     } catch (error) {
       console.log(error);
     }
