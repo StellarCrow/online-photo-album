837e4144e9be0deed33c24187aba092f05479eb5
StellarCrow <dariadar12@gmail.com>
2019-12-22 23:38:09 +0200

    scss <style> fixes

--- client/src/App.vue
+++ client/src/App.vue
@@ -23,5 +23,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import "styles/global.scss";
+@import "normalize-scss";
+@include normalize();
+@import url("https://fonts.googleapis.com/css?family=Comfortaa:300,400,600,700|Poiret+One&display=swap&subset=cyrillic");
+@import url("https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&display=swap&subset=cyrillic");
 </style>
