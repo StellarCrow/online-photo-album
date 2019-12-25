2ca03e0f020b6deb6981f4705192eb9b868c3bf8
StellarCrow <dariadar12@gmail.com>
2019-12-25 23:22:34 +0200

    setup express js app

---
+++ server/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
